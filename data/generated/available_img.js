ddbacdbcf73b3c5f2ca61ba7a223fcfe46ffb395
ssmghub <98471266+ssmghub@users.noreply.github.com>
2023-05-14 03:19:54 +0100

    Add files via upload

---
+++ data/generated/available_img.js
@@ -0,0 +1,1 @@
+const availableImg = ["pavlin2019opinions","li2022application","al2023analyzing","remolar2021learning","soltani2020augmented","demitriadou2020comparative","richard2007augmented","sun2023effects","tsekhmister2021evaluation","moro2021improving"];
